fef01e766c4988755d1e77b977c7992f2758cec4
codedust <codedust@users.noreply.github.com>
2016-02-07 04:18:03 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,25 @@
+HTML5 file browser
+---
+
+This HTML5 file browser allows to browse and share files in a public server directory. Unlike some other solutions out there, this file browser does not depend on any server-side code. Instead, directory listings are used to parse the list of available files.
+
+### Configuration
+#### Apache
+This program comes with a `.htaccess` file that enables directory listings for the `files/` directory. Simply upload this file browser to any directory on your server and you are done.
+
+#### Nginx
+For `nginx`, auto-indexing has to be turned on for the `/files/` directory:
+```
+location /somedir/files/ {
+    autoindex on;
+}
+```
+
+#### Python
+For testing purposes or to share files in a LAN, Python can be used. Run `python -m http.server 8080` (or `python2 -m SimpleHTTPServer 8080` if your system is  horribly outdated) from within this directory (the directory where `README.md` can be found) and you are done.
+
+### License
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
---
+++ app.js
@@ -0,0 +1,159 @@
+var app = function(){
+  var base_dir = (location.pathname.replace('/index.html', '/') +
+                  "/files/").replace(/\/\//g, '/');
+  var current_dir = (base_dir + location.hash.substring(1) +
+                     '/').replace(/\/\//g, '/');
+  var IMG_EXTENSIONS = ['bmp', 'gif', 'jpg', 'jpeg', 'jpe', 'png'];
+  var IGNORED_ELEMENTS = ['../', 'Name', 'Last modified', 'Size', 'Description',
+                          'Parent Directory'];
+  var imgCache = [];
+  var prev_img = "";
+  var next_img = "";
+
+
+  // create a tile
+  function createTile(href, name) {
+    return '<a href="'+href+name+'"><span class="glyphicon glyphicon-file" aria-hidden="true"></span>'+name+'</a>';
+  }
+
+  // cache an image for future usage
+  function cacheImage(file) {
+    for (var i=0; i<imgCache.length; i++) {
+      if (imgCache[i].src == file) return;
+    }
+    imgCache.push(file);
+  }
+
+  // check if the given path points to an image
+  function isImage(path) {
+    return $.inArray(path.split('.').pop().toLowerCase(), IMG_EXTENSIONS) != -1;
+  }
+
+  function isValidTile(name) {
+    return $.inArray(name, IGNORED_ELEMENTS) == -1;
+  }
+
+  // load the contents of the given directory
+  function cd(dir) {
+    current_dir = dir;
+
+    console.log(current_dir, base_dir);
+    location.hash = current_dir.replace(base_dir, '');
+
+    // show the location bar
+    $(".current-dir").text('');
+    var path = current_dir.replace(base_dir, '/').split('/');
+
+    var temp_path = "";
+    for (var i=0; i<path.length-1; i++) {
+      var a = document.createElement('a');
+      temp_path += path[i] + '/';
+      $(a).text(path[i] + '/');
+      a.title = base_dir + temp_path.substring(1);
+      $(a).click(function(){
+        cd(this.title);
+      });
+      $(".current-dir").append(a);
+    }
+
+    // retrieve the contents of the directory
+    $.get(current_dir, function(data) {
+      html = $.parseHTML(data);
+      $(".browser-view").html("");
+
+      // create tiles
+      $(html).find("a").each(function(i, element){
+        if (isValidTile(element.text)) {
+          $(".browser-view").append(
+            createTile(current_dir, element.text));
+        }
+      });
+
+      // add events to tiles
+      $(".browser-view a").each(function(i, element){
+        if (element.pathname.slice(-1) == "/" ) {
+          // open directories
+          $(element).click(function(e) {
+            e.preventDefault();
+            cd(element.pathname);
+          });
+        } else if (isImage(element.pathname)) {
+          // show image previews
+          $(element).click(function(e) {
+            e.preventDefault();
+            showPreview(element.pathname);
+          });
+        }
+      });
+    });
+  }
+
+  // show an image preview of the given file
+  function showPreview(filepath){
+    $(".bg-translucent").css('display', 'block');
+    $(".file-view-img").attr('src', 'loader.gif');
+    $(".file-view-wrapper").css('display', 'block');
+    var img = new Image();
+    img.src = filepath;
+    img.onload = function() {
+      $(".file-view-img").attr('src', filepath);
+      var imgWidth = $('.file-view-img').width();
+      $(".file-view-wrapper").css('left', ($(document).width()-imgWidth)/2);
+      $(".file-view-wrapper").css('width', imgWidth);
+      $(".file-view-prev").css('display', 'block');
+      $(".file-view-next").css('display', 'block');
+    };
+    cacheImage(filepath);
+
+    // search for the previous and next image to be displayed
+    var first_img = "";
+    var last_img = "";
+    prev_img = "";
+    next_img = "";
+    var img_found = false;
+    $(".browser-view a").each(function(i, element){
+      if (isImage(element.pathname)) {
+        if (first_img === "") first_img = element.pathname;
+        if (img_found && next_img === "") { next_img = element.pathname; }
+        if (element.pathname == filepath) img_found = true;
+        if (!img_found) prev_img = element.pathname;
+        last_img = element.pathname;
+      }
+    });
+    if (next_img === "") next_img = first_img;
+    if (prev_img === "") prev_img = last_img;
+  }
+
+  // close the image preview
+  function closePreview() {
+    $(".bg-translucent").css('display', 'none');
+    $(".file-view-wrapper").css('display', 'none');
+  }
+
+  // add various event handlers
+  $('.file-view-prev').click(function(){
+    showPreview(prev_img);
+  });
+  $('.file-view-next').click(function(){
+    showPreview(next_img);
+  });
+  $("body").keydown(function(event) {
+    switch (event.which) {
+      case 27: // ESC
+        closePreview();
+        break;
+      case 37: // left arrow key
+        showPreview(prev_img);
+        break;
+      case 39: // right arrow key
+        showPreview(next_img);
+        break;
+    }
+  });
+  $(".bg-translucent").click(closePreview);
+  $('.base-dir-icon').click(function(){
+    cd(base_dir);
+  });
+
+  cd(current_dir);
+}();
